abe01c9d2514abd993f0c92fe84db3142855313b
Taufan Augusta <taufan.augusta@protonmail.com>
2022-03-06 12:26:07 +0700

    add UserAdapter mapper

---
+++ src/Application/Mappers/UserAdapter.cs
@@ -0,0 +1,21 @@
+using Mapster;
+using Archable.Application.Helpers.Security;
+using Archable.Application.Models.Account;
+using Archable.Domain.Entities.Account;
+
+namespace Archable.Application.Mappers
+{
+    public static class UserAdapter
+    {
+        public static User AdaptToUser(this NewUser newUser)
+        {
+            var adapter = new TypeAdapterConfig()
+                .NewConfig<NewUser, User>()
+                .Map(dst => dst.Password, src => Hasher.GetHash(src.Password, src.Salt));
+
+            var user = newUser.Adapt<User>(adapter.Config);
+
+            return user;
+        }
+    }
+}
