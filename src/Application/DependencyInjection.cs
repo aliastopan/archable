e8db8a3310d3d5d8a0f48b14b51ee08a7935e5f8
Taufan Augusta <taufan.augusta@protonmail.com>
2022-03-06 17:43:03 +0700

    add NewUserValidator

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,5 +1,7 @@
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using Archable.Application.Controllers;
+using Archable.Application.Validators;
 
 namespace Archable.Application
 {
@@ -8,6 +10,8 @@ namespace Archable.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddTransient<UserController>();
+
+            services.AddValidatorsFromAssemblyContaining<NewUserValidator>(ServiceLifetime.Transient);
 
             return services;
         }
