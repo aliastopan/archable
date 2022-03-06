3855bdc3031a3d2c3e42091499f3485b1b9e43b9
Taufan Augusta <taufan.augusta@protonmail.com>
2022-03-06 18:04:18 +0700

    refactor: map Role and AccountFlag

--- src/Application/Mappers/UserAdapter.cs
+++ src/Application/Mappers/UserAdapter.cs
@@ -1,5 +1,6 @@
 using Mapster;
 using Archable.Application.Helpers.Security;
+using Archable.Application.Enums.Account;
 using Archable.Application.Models.Account;
 using Archable.Domain.Entities.Account;
 
@@ -11,6 +12,8 @@ namespace Archable.Application.Mappers
         {
             var adapter = new TypeAdapterConfig()
                 .NewConfig<NewUser, User>()
+                .Map(dst => dst.Role, src => Role.CastFrom<Role>(src.RoleSelect).Name)
+                .Map(dst => dst.Flag, src => AccountFlag.CastFrom<AccountFlag>(src.FlagSelect).Name)
                 .Map(dst => dst.Password, src => Hasher.GetHash(src.Password, src.Salt));
 
             var user = newUser.Adapt<User>(adapter.Config);
