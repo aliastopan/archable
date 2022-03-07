c83fe4ba998a9136875f9566667b48b36059cb44
Taufan Augusta <taufan.augusta@protonmail.com>
2022-03-07 13:45:49 +0700

    remove ExceptionMessage

--- src/Application/Exceptions/ExceptionMessage.cs
+++
@@ -1,9 +0,0 @@
-namespace Archable.Application.Exceptions
-{
-    public static class ExceptionMessage
-    {
-        public const string FAIL = "FAIL";
-        public const string ENTITY_NOT_FOUND = "ENTITY NOT FOUND";
-        public const string USER_NOT_FOUND = "USER NOT FOUND";
-    }
-}
