44230dc55b399ec4a1f20b99ca3b74ea907fa471
Taufan Augusta <taufan.augusta@protonmail.com>
2022-03-08 12:31:25 +0700

    refactor: add Serializable attribute

--- src/Application/Exceptions/EntityNotFoundException.cs
+++ src/Application/Exceptions/EntityNotFoundException.cs
@@ -1,11 +1,20 @@
+using System.Runtime.Serialization;
+
 namespace Archable.Application.Exceptions
 {
+    [Serializable]
     public class EntityNotFoundException : Exception
     {
         public EntityNotFoundException()
             :base("Not Found: ENTITY exception.") { }
 
         public EntityNotFoundException(string message)
-            :base(message) { }
+            : base(message) { }
+
+        public EntityNotFoundException(string message, Exception inner)
+            : base(message, inner) { }
+
+        protected EntityNotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
     }
 }
